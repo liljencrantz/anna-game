8914a3b1df22219a51120d1a1264545ff12d5ca6
Egil Moeller <egil.moller@freecode.no>
2010-11-14 18:19:03 +0100

    Lua overrides

---
+++ luauser.c
@@ -0,0 +1,16 @@
+#include <lua5.1/lua.h>
+#include <lua5.1/lauxlib.h>
+#include <lua5.1/lualib.h>
+
+void annalua_lock(lua_State * L) {
+}
+
+void annalua_unlock(lua_State * L) {
+}
+
+void annalua_userstateopen(lua_State * L) {
+}
+
+void annalua_userstatethread(lua_State * L, lua_State * L1) {
+}
+
